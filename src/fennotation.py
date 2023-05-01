2501bc8e60938c71a6d9f8344e2d37e2ba975f43
mismorgano <mismorgano@gmail.com>
2023-04-30 22:03:14 -0600

    Refactoring and documentation
    Just a little, basically it's the same functionality but "clean".
    There're some bugs.
    
    TODO:
    
    - Fix the piece's moves
    - Update the board state and fen notation
    - Show legal moves

--- src/fennotation.py
+++ src/fennotation.py
@@ -1,27 +1,28 @@
-
 class Fen:
+    """Represent the Fen notation of the board state"""
 
-	def __init__(self, fen: str) -> None:
-		self._fen = fen
-		self._board = []
-		ranks = fen.split('/')
-		for rank in ranks:
-			file = []
-			for _, piece in enumerate(rank):
-				if not piece.isdigit():
-					file.append(piece)
-				else:
-					file += [None] * int(piece)
-			self._board.append(file)
-		self._board = list(reversed(self._board))
-		print(self._board)
+    def __init__(self, fen: str) -> None:
+        """Constructs the board state given a valid fen string"""
+        self._fen = fen
+        self._board = []
+        ranks = fen.split("/")
+        for rank in ranks:
+            file = []
+            for _, piece in enumerate(rank):
+                if not piece.isdigit():
+                    file.append(piece)
+                else:
+                    file += [None] * int(piece)
+            self._board.append(file)
+        # reversed to use [rank][file] indexing
+        self._board = list(reversed(self._board))
+        # print(self._board)
+
+    def __getitem__(self, position):
+        return self._board[position]
 
 
-	def __getitem__(self, position):
-		return self._board[position]
+fen = Fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
-
-fen = Fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-
-print(list(reversed(fen._board)))
-print(len(fen._board))
+# print(list(reversed(fen._board)))
+# print(len(fen._board))
